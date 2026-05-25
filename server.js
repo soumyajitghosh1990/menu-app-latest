e03063894362c9ed766749b66d92363a854781e5
soumyajitghosh1990 <soumyajitghosh1990@gmail.com>
2026-05-25 12:52:05 +0530

    Enhance frontend HTML with active item form insertion and deletion UI handles fix error

--- server.js
+++ server.js
@@ -250,7 +250,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
